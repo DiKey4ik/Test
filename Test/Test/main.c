f1d01386470a9e3af0c9f41c2752ee195288968e
Дмитрий Костюк <kostukdmitry@gmail.com>
2024-10-27 17:24:12 +0300

    Add index

---
+++ Test/Test/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Test
+//
+//  Created by Dmitry Kostuk on 27.10.2024.
+//  Copyright © 2024 Dmitry Kostuk. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
